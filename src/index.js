7ed75d3ac9bd0e24224f7974e336f45443a24f8a
Sumit Chahal <smtchahal@gmail.com>
2018-09-29 20:13:31 +0530

    Add redux first router functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
 import AppContainer from './AppContainer';
+import connectedRoutes from './connectedRoutes';
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const { middleware, enhancer } = connectedRoutes;
+
+const middlewares = applyMiddleware(middleware);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(enhancer, middlewares));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
 import selectedCharacter from './selectedCharacter';
 import characters from './characters';
+import connectedRoutes from '../connectedRoutes';
 
-export default combineReducers({ selectedCharacter, characters });
+const { reducer: location } = connectedRoutes;
+
+export default combineReducers({ selectedCharacter, characters, location });
